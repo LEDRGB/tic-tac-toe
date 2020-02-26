9f6eb5b50744c8382d4fdb8c78e1429c71e3cec4
ledrgb <hector.de.luis.g@gmail.com>
2020-02-26 14:23:47 +0100

    Añadidos fondos

--- components/home.js
+++ components/home.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, TouchableHighlight, TouchableWithoutFeedback, Button, Text, Alert } from 'react-native'
+import { View, StyleSheet, TouchableWithoutFeedback, Button, Text } from 'react-native'
 import Modal from "react-native-modal";
-import { Col, Row, Grid } from 'react-native-easy-grid';
+import { Grid } from 'react-native-easy-grid';
 import { Image  } from 'react-native-elements'
 import {
-   AdMobBanner,
-   AdMobInterstitial,
-   PublisherBanner,
-   AdMobRewarded
+   AdMobBanner
  } from 'expo-ads-admob';
 // import cross from '../assets/cross.png';
 // import circle from '../assets/circle.png';
@@ -20,6 +17,15 @@ export default class App extends Component {
       selectedInput: "o",
       cross: require('../assets/cross.png'),
       circle: require('../assets/circle.png'),
+      backGroundCounter: 1,
+      backGrounds: [
+         require('../assets/fondoPlanetas.jpg'),
+         require('../assets/fondoluna.jpg'),
+         require('../assets/fondoSuperheroes.jpg'),
+         require('../assets/naturaleza.jpg'),
+         require('../assets/art.jpg')
+      ],
+      backGround:  require('../assets/fondoPlanetas.jpg'),
       transparent: require('../assets/transparent.png'),
       isModalVisible: false,
       modalText: "The winner is no one."
@@ -71,9 +77,17 @@ export default class App extends Component {
    toggleModal = () => {
       this.setState({ isModalVisible: !this.state.isModalVisible });
    };
+   changeBackground = () => {
+      let counter = this.state.backGroundCounter +1
+      if(this.state.backGroundCounter >= this.state.backGrounds.length -1){
+         counter = 0
+      }
+      this.setState({ backGround: this.state.backGrounds[counter], backGroundCounter : counter });
+   }
    render(){
       return (
-         <View>
+         <Image source={this.state.backGround} style={styles.backGround}>
+            <View style={styles.containerView}>
             <View style={styles.playerMarker}>
             {this.state.player ?  <Text style={styles.titlePlayerP2}>{"P2"}</Text> :<Text style={styles.titlePlayerP1}>{"P1"}</Text>}   
             </View>
@@ -87,64 +101,64 @@ export default class App extends Component {
                   </TouchableWithoutFeedback>
                </View>
                <View style = {styles.whitebox}>
-               <TouchableHighlight onPress={() => this.onPressButton(1)}>
+               <TouchableWithoutFeedback onPress={() => this.onPressButton(1)}>
                      <View>
                      {this.state.gameStatus[1] ? <Image  source={this.state.gameStatus[1] === 'x' ? this.state.cross : this.state.circle } style={{ width: 100, height: 100 }}/> : <Image  source={this.state.transparent} style={{ width: 100, height: 100 }}/>}
                      </View>
-                  </TouchableHighlight>
+                  </TouchableWithoutFeedback>
                </View>
                <View style = {styles.whitebox}>
-               <TouchableHighlight onPress={() =>this.onPressButton(2)}>
+               <TouchableWithoutFeedback onPress={() =>this.onPressButton(2)}>
                      <View>
                      {this.state.gameStatus[2] ? <Image  source={ this.state.gameStatus[2] === 'x' ? this.state.cross : this.state.circle } style={{ width: 100, height: 100 }}/> : <Image  source={this.state.transparent} style={{ width: 100, height: 100 }}/>}
                      </View>
-                  </TouchableHighlight>
+                  </TouchableWithoutFeedback>
                </View>            
             </View>
             <View style = {styles.container}>
             <View style = {styles.whitebox} >
-                  <TouchableHighlight onPress={() =>this.onPressButton(3)}>
+                  <TouchableWithoutFeedback onPress={() =>this.onPressButton(3)}>
                      <View>
                      {this.state.gameStatus[3] ? <Image  source={ this.state.gameStatus[3] === 'x' ? this.state.cross : this.state.circle } style={{ width: 100, height: 100 }}/> : <Image  source={this.state.transparent} style={{ width: 100, height: 100 }}/>}
                      </View>
-                  </TouchableHighlight>
+                  </TouchableWithoutFeedback>
                </View>
                <View style = {styles.whitebox}>
-               <TouchableHighlight onPress={() =>this.onPressButton(4)}>
+               <TouchableWithoutFeedback onPress={() =>this.onPressButton(4)}>
                      <View>
                      {this.state.gameStatus[4] ? <Image  source={ this.state.gameStatus[4] === 'x' ? this.state.cross : this.state.circle } style={{ width: 100, height: 100 }}/> : <Image  source={this.state.transparent} style={{ width: 100, height: 100 }}/>}
                      </View>
-                  </TouchableHighlight>
+                  </TouchableWithoutFeedback>
                </View>
                <View style = {styles.whitebox}>
-               <TouchableHighlight onPress={() =>this.onPressButton(5)}>
+               <TouchableWithoutFeedback onPress={() =>this.onPressButton(5)}>
                      <View>
                      {this.state.gameStatus[5] ? <Image  source={ this.state.gameStatus[5] === 'x' ? this.state.cross : this.state.circle } style={{ width: 100, height: 100 }}/> : <Image  source={this.state.transparent} style={{ width: 100, height: 100 }}/>}
                      </View>
-                  </TouchableHighlight>
+                  </TouchableWithoutFeedback>
                </View> 
             </View>
             <View style = {styles.container} >
                <View style = {styles.whitebox} >
-                     <TouchableHighlight onPress={() =>this.onPressButton(6)}>
+                     <TouchableWithoutFeedback onPress={() =>this.onPressButton(6)}>
                         <View>
                         {this.state.gameStatus[6] ? <Image  source={ this.state.gameStatus[6] === 'x' ? this.state.cross : this.state.circle } style={{ width: 100, height: 100 }}/> : <Image  source={this.state.transparent} style={{ width: 100, height: 100 }}/>}
                         </View>
-                     </TouchableHighlight>
+                     </TouchableWithoutFeedback>
                   </View>
                   <View style = {styles.whitebox}>
-                     <TouchableHighlight onPress={() =>this.onPressButton(7)}>
+                     <TouchableWithoutFeedback onPress={() =>this.onPressButton(7)}>
                         <View>
                         {this.state.gameStatus[7] ? <Image  source={ this.state.gameStatus[7] === 'x' ? this.state.cross : this.state.circle } style={{ width: 100, height: 100 }}/> : <Image  source={this.state.transparent} style={{ width: 100, height: 100 }}/>}
                         </View>
-                     </TouchableHighlight>
+                     </TouchableWithoutFeedback>
                   </View>
                   <View style = {styles.whitebox}>
-                  <TouchableHighlight onPress={() =>this.onPressButton(8)}>
+                  <TouchableWithoutFeedback onPress={() =>this.onPressButton(8)}>
                      <View>
                      {this.state.gameStatus[8] ? <Image  source={ this.state.gameStatus[8] === 'x' ? this.state.cross : this.state.circle } style={{ width: 100, height: 100 }}/> : <Image  source={this.state.transparent} style={{ width: 100, height: 100 }}/>}
                      </View>
-                  </TouchableHighlight>
+                  </TouchableWithoutFeedback>
                </View>
             </View>                       
             </Grid>
@@ -153,7 +167,7 @@ export default class App extends Component {
                      <Button title="Restart" onPress={() => {this.setState({gameStatus: [], player: false, selectedInput: "o"}); this.toggleModal(); }} />
                      <AdMobBanner
                         bannerSize="fullBanner"
-                        adUnitID= "ca-app-pub-9330146669440542/5854606848"//"ca-app-pub-9330146669440542/5854606848" // ca-app-pub-3940256099942544/6300978111
+                        adUnitID= "ca-app-pub-9330146669440542/5854606848"
                         testDeviceID="EMULATOR"
                         servePersonalizedAds // true or false
                         onDidFailToReceiveAdWithError={this.bannerError} 
@@ -161,14 +175,9 @@ export default class App extends Component {
                      <Text style={styles.titleText}>{this.state.modalText}</Text>
                   </View>
                </Modal>
-              
-            {/* <Button
-               title="Restart"
-               // color="#f194ff"
-               onPress={() => {this.setState({gameStatus: [], player: false, selectedInput: "o"}); this.toggleModal()}}               
-               // onPress={() => Alert.alert('Button with adjusted color pressed')}
-            /> */}
-         </View>         
+               <Button title="Change background" onPress={() => {this.changeBackground()}} />
+               </View>
+         </Image>         
       )
    }
 }
@@ -180,6 +189,22 @@ const styles = StyleSheet.create ({
       marginTop: -400,
       flexGrow : 1,
    },
+   backGround: {
+      flex: 1,
+      width: 500,
+      resizeMode: 'stretch',
+      flexDirection: 'column',
+      justifyContent: 'center',      
+      alignItems: 'center',
+      
+    },
+    containerView: {
+      flex: 1,
+      //resizeMode: 'cover',
+      // remove width and height to override fixed static size
+      width: 300,
+      height: null,
+    },
    containerCol: {
       flexDirection: 'column',
       justifyContent: 'space-around',
